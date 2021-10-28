0be19b9278249cc6d38fe8fa011e518bae4385d7
RyuujiAkira <jrabajac@gmail.com>
2021-10-28 01:13:04 +0100

    Update sql-schema.sql
    Updated schema for system.

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE IF NOT EXISTS `ims`.`customers` (
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
 );
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `product_name` VARCHAR(40) DEFAULT NULL,
+    `value` FLOAT(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `customer_id` INT(40) DEFAULT NULL,
+    `order_items_id` INT(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_items` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `item_id` INT(40) DEFAULT NULL,
+    `quantity` INT(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
